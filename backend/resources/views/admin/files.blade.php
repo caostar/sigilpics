fbed09df872f2cc324694d5f2a8e84286cdea224
caostar <ad@iot-sulamerica.com.br>
2016-08-11 00:10:53 -0300

    already fully connected with google drive api

---
+++ backend/resources/views/admin/files.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.default')
+
+@section('content')
+<h3>List Files</h3>
+<ul id="files">
+    @foreach($files as $file)
+    <li>
+        <div class="file">
+
+            <div class="file-title">
+                <img src="{{ $file->iconLink }}">
+                {{ $file->name }}
+            </div>
+            <div class="file-modified">
+                last modified: {{ Date::format($file->modifiedTime) }}
+            </div>
+            <div class="file-links">
+                <a href="{{ $file->webViewLink }}">view</a>
+                @if(!empty($file->webContentLink))
+                <a href="{{ $file->webContentLink }}">download</a>
+                @endif
+                <a href="delete/{{ $file->id }}">delete</a>
+            </div>
+        </div>
+    </li>
+    @endforeach
+</ul>
+@stop
