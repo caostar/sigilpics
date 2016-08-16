fd38c38329c206d432d175ceb0999fe2e824de19
caostar <ad@iot-sulamerica.com.br>
2016-08-16 19:17:34 -0300

    encapsulated google login check to force it on every call to the api

--- backend/app/Googl.php
+++ backend/app/Googl.php
@@ -3,6 +3,9 @@
 //https://github.com/laracasts/flash
 namespace App;
 
+use Illuminate\Http\Request;
+use App\Token;
+
 class Googl
 {
     public function client()
@@ -24,4 +27,70 @@ class Googl
         $drive = new \Google_Service_Drive($client);
         return $drive;
     }
+
+    public function authenticateOnDrive(Request $request)
+    {
+        $client = $this->client();
+
+        if ($request->has('code')) {
+
+            $client->authenticate($request->input('code'));
+            //$token = $client->getAccessToken();
+            //
+            $tokenModel = new Token();
+            $tokenModel->plataform = 'googledrive';
+            $tokenModel->token = json_encode($client->getAccessToken());
+            $tokenModel->save();
+            $token = Token::first();
+
+        } else if (!Token::where('plataform', 'googledrive')->first()){
+            $auth_url = $client->createAuthUrl();
+            return $auth_url;
+        }else{
+            $token = Token::where('plataform', 'googledrive')->first();
+            //check if we need to refresh the token
+            $client->setAccessToken($token->token);
+            if ($client->isAccessTokenExpired()) {
+                //print_r("Ok. Token expirado. Vamos renovar.<br/><br/>");
+                $tokenData = $client->getAccessToken();
+                //print_r("<br/><br/>tokenData:<br/>");
+                //print_r($tokenData);
+                $refreshToken = $tokenData['refresh_token'];
+                //print_r("<br/><br/>refreshToken:<br/>");
+                //print_r($refreshToken);
+                $client->refreshToken($refreshToken);
+
+                //print_r("<br/><br/>npvp token:<br/>");
+                $newToken = $client->getAccessToken();
+                $newToken['refresh_token'] = $refreshToken;
+                //print_r($newToken);
+
+                $token->token = json_encode($newToken);
+                $token->save();
+
+            }
+        }
+        //
+        /*$plus = new \Google_Service_Plus($client);
+
+        $google_user = $plus->people->get('me');
+        $id = $google_user['id'];
+
+        $email = $google_user['emails'][0]['value'];
+        $first_name = $google_user['name']['givenName'];
+        $last_name = $google_user['name']['familyName'];*/
+
+        session([
+            'user' => [
+                /*'email' => $email,
+                'first_name' => $first_name,
+                'last_name' => $last_name,*/
+                'token' => $token->token
+            ]
+        ]);
+
+        flash('You are now logged in.');
+        return 'dashboard';
+
+   }
 }
--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -16,66 +16,6 @@ class HomeController extends Controller
 
     public function login(Googl $googl, Request $request)
     {
-        $client = $googl->client();
-
-        if ($request->has('code')) {
-
-            $client->authenticate($request->input('code'));
-            //$token = $client->getAccessToken();
-            //
-            $tokenModel = new Token();
-            $tokenModel->plataform = 'googledrive';
-            $tokenModel->token = json_encode($client->getAccessToken());
-            $tokenModel->save();
-            $token = Token::first();
-
-        } else if (!Token::where('plataform', 'googledrive')->first()){
-            $auth_url = $client->createAuthUrl();
-            return redirect($auth_url);
-        }else{
-            $token = Token::where('plataform', 'googledrive')->first();
-            //check if we need to refresh the token
-            $client->setAccessToken($token->token);
-            if ($client->isAccessTokenExpired()) {
-                //print_r("Ok. Token expirado. Vamos renovar.<br/><br/>");
-                $tokenData = $client->getAccessToken();
-                //print_r("<br/><br/>tokenData:<br/>");
-                //print_r($tokenData);
-                $refreshToken = $tokenData['refresh_token'];
-                //print_r("<br/><br/>refreshToken:<br/>");
-                //print_r($refreshToken);
-                $client->refreshToken($refreshToken);
-
-                //print_r("<br/><br/>npvp token:<br/>");
-                $newToken = $client->getAccessToken();
-                $newToken['refresh_token'] = $refreshToken;
-                //print_r($newToken);
-
-                $token->token = json_encode($newToken);
-                $token->save();
-
-            }
-        }
-        //
-        /*$plus = new \Google_Service_Plus($client);
-
-        $google_user = $plus->people->get('me');
-        $id = $google_user['id'];
-
-        $email = $google_user['emails'][0]['value'];
-        $first_name = $google_user['name']['givenName'];
-        $last_name = $google_user['name']['familyName'];*/
-
-        session([
-            'user' => [
-                /*'email' => $email,
-                'first_name' => $first_name,
-                'last_name' => $last_name,*/
-                'token' => $token->token
-            ]
-        ]);
-
-        flash('You are now logged in.');
-        return redirect('dashboard');
+        return redirect($googl->authenticateOnDrive($request));
    }
 }
