2170fc93b56fc90259938efa70bd1ee3864e0ec0
caostar <ad@iot-sulamerica.com.br>
2017-01-19 10:59:41 -0200

    fullscreen support

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,6 +3,7 @@ import {config} from '../package.json';
 import Renderer from './Renderer/Renderer';
 import App from './displayobjects/App/App';
 import AnimationStore from './stores/AnimationStore';
+import screenfull from 'screenfull'
 
 const renderer = new Renderer(config.stageWidth, config.stageHeight);
 const app = new App(config.stageWidth, config.stageHeight);
@@ -11,3 +12,12 @@ document.body.appendChild(renderer.view);
 
 renderer.addRenderable(app);
 renderer.start();
+
+document.body.addEventListener('keyup', (e) => {
+    //key "F"
+    if (e.keyCode==70 && screenfull.enabled) {
+        screenfull.toggle();
+    } else {
+        alert("Fullscreen not supported.");
+    }
+});
