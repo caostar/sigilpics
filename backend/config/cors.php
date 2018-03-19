bb46d5db4ed3ad2dbb21372c8d2fab5efeb4f3ae
caostar <ad@iot-sulamerica.com.br>
2018-03-19 12:14:27 -0300

    add OPTIONS to CORs

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,7 +14,7 @@ return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
-    'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
+    'allowedMethods' => ['OPTIONS', 'GET', 'POST', 'PUT',  'DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
     'hosts' => [],
